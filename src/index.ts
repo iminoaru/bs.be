0d357b7b2d3b0a49f2e9e5f6f5e217c009b3e2f7
iminoaru <sarthakgaud04@gmail.com>
2026-03-09 12:15:36 +0530

    path for deployment

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,6 @@ const app = express();
 app.use(express.json());
 
 import path from 'path';
-// Use process.cwd() instead of __dirname to ensure Vercel finds the folder correctly from the project root
 app.use(express.static(path.join(process.cwd(), 'public')));
 
 app.use('/', routes);
